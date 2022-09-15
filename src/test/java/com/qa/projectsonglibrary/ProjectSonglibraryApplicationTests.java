b42df85abb95439e8bf8831a7715d2f1cecc9a4d
lanyamd2 <laniangel12@gmail.com>
2022-09-15 13:28:05 +0100

    1st git commit - please note the project is not complete yet.
    Jira/Miro Documentation/User Stories -> incomplete
    ERD -> incomplete
    MySQL Server Connection -> complete
    Front End: HTML, CSS, JavaScript -> incomplete
    JUnit Testing -> incomplete

---
+++ src/test/java/com/qa/projectsonglibrary/ProjectSonglibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.projectsonglibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSonglibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
